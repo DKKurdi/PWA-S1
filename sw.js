8af529de33b6e7019f9d9da28416bcb8fe333d31
DKKurdi <128648584+DKKurdi@users.noreply.github.com>
2025-05-15 16:24:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,36 @@
+const CACHE_NAME = 'my-cache-v1';
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/script.js',
+    '/style.css',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/script.js',
-                '/style.css'
-            ]);
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
         })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
     );
